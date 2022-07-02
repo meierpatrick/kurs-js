58a6e4c9ec20a6d73e2f0c9288c7d5bf8ae3a397
dev <dev@Patricks-MacBook-Pro.local>
2022-07-02 12:47:22 +0300

    Added my project

---
+++ script.js
@@ -0,0 +1,106 @@
+/*
+et myCountry = "Deutschland";
+let myContinent = "Europe";
+let myPopulation = 100;
+
+console.log(myCountry, myContinent, myPopulation);
+*/
+
+/*
+// Practice
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Patrick");
+
+javascriptIsFun = "YES!";
+
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(typeof year);
+
+year = 1991;
+
+console.log(typeof year);
+
+console.log(typeof null);
+
+// Assignment
+
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+*/
+
+/*
+// Mutate variable
+let age = 28;
+age = 31;
+
+// Unmuteable variable
+const birthday = 1991;
+// birthday = 1990;
+// const job;
+
+var job = "programmer";
+job = "teacher";
+
+// Never declare variable like this
+lastName = "Meier";
+console.log(lastName);
+
+// Assignment
+language = "deutsch";
+const country = "Deutschland";
+const continent = "Europe";
+const isIsland = false;
+isIsland = true;
+*/
+
+/*
+// Operators
+
+// Math operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = "Patrick";
+const lastName = "Meier";
+console.log(firstName + " " + lastName);
+
+// Assignment operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1 = 101
+x--; // x = x - 1 = 100
+x--; // x = x - 1 = 99
+console.log(x);
+
+// Comparison operators
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+*/
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
